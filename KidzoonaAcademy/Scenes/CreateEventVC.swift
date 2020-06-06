de1a9aa8d23706bcf731515df69838bf66553c45
Marwan Magdy <58157005+MarwanMagdy1295@users.noreply.github.com>
2020-06-06 17:53:56 +0200

    createEvent

--- KidzoonaAcademy/Scenes/CreateEventVC.swift
+++ KidzoonaAcademy/Scenes/CreateEventVC.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateEvent: UIViewController {
+    
+    private var datePicker: UIDatePicker?
+    var eventImgString = ""
+    
     @IBOutlet weak var DiscountTxt: RoundedTextField!
-    
     @IBOutlet weak var seatTxt: RoundedTextField!
+    @IBOutlet weak var eventName: RoundedTextField!
+    @IBOutlet weak var coach: RoundedTextField!
+    @IBOutlet weak var price: RoundedTextField!
+    @IBOutlet weak var date: RoundedTextField!
+    @IBOutlet weak var time: RoundedTextField!
+    @IBOutlet weak var txtDescription: RoundedTextField!
+    @IBOutlet weak var eventImg: UIButton!
     
+    @IBAction func addImg(_ sender: Any) {
+        
+        presentImgPicker()
+    }
+    
+    
+    @IBAction func createAction(_ sender: Any) {
+        
+        uploadEvent()
+    }
+    
+    
+    
+    //increaseDiscountDiscount
     @IBAction func increaseDiscount(_ sender: Any) {
         print(DiscountTxt.text as Any)
         if (Int(DiscountTxt.text?.dropLast() ?? "0") ?? 0 < 95)
@@ -21,15 +46,14 @@ class CreateEvent: UIViewController {
         print(Currentdisc as Any)
         DiscountTxt.text = String(Currentdisc) + "%"
             print(DiscountTxt.text as Any )
-            
         }
             else
         {
                 DiscountTxt.text =  "100%"
                 
             }
-        
     }
+    //decreaseDiscountDiscount
     @IBAction func decreaseDiscount(_ sender: Any) {
         print(DiscountTxt.text as Any)
         if (Int(DiscountTxt.text?.dropLast() ?? "0") ?? 0 > 5)
@@ -42,12 +66,10 @@ class CreateEvent: UIViewController {
         else
         {
             DiscountTxt.text =  "0%"
-
-            
         }
     }
     
-
+    //increaseSeats
     @IBAction func increaseSeats(_ sender: Any)
     {
         print(seatTxt.text as Any)
@@ -62,7 +84,7 @@ class CreateEvent: UIViewController {
         
     }
     
-    
+    //decreaseSeats
     @IBAction func decreaseSeats(_ sender: Any) {
         print(seatTxt.text as Any)
         if (Int(seatTxt.text ?? "0") ?? 0 > 1)
@@ -75,31 +97,87 @@ class CreateEvent: UIViewController {
         else
         {
             seatTxt.text =  "0"
-            
-            
         }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setView()
         DiscountTxt.text = "5%"
         DiscountTxt.isEnabled = false
         
         self.navigationController?.isNavigationBarHidden = true
-
-
         // Do any additional setup after loading the view.
-    }
-    
-    
-    func setView(){
-
         
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(CreateEvent.dateChanged(datePicker:)),
+                              for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action:
+            #selector(CreateEvent.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+         date.inputView = datePicker
     }
     
-
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker) {
+        
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "MM/dd/yyyy"
+        date.text = dateformatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+    
+    func presentImgPicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+                eventImg.setImage(pickedImage, for: .normal)
+                eventImg.contentMode = .scaleAspectFit
+            }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //upload Event Details To DataBase
+    func uploadEvent(){
+        
+        if eventName.text! == "" && price.text! == "" && date.text! == "" && time.text! == ""
+        {
+            AlertController.showAlert(inViewController: self, title: "Alert", message: "Please fill out requered fields.")
+        }
+        else {
+            
+        let databaseRoot = Database.database().reference()
+        let academiesRoot = databaseRoot.child("Academies")
+        let userId = Auth.auth().currentUser?.uid
+        let academyId = academiesRoot.child(userId!)
+        let eventsRoot = academyId.child("Events")
+        let eventId = eventsRoot.childByAutoId()
+        let eventToUpload = ["name":eventName.text!,"date":date.text!,"time":time.text!,"coach":coach.text!,
+                             "description":txtDescription.text!,"price":price.text!,"availableSeats":seatTxt.text!,
+                             "discount":DiscountTxt.text!] as [String : Any]
+        eventId.setValue(eventToUpload)
+    }
+    
+}
+    
     /*
     // MARK: - Navigation
 
